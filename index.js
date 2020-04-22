7e7699c4a86289a962980a160edd8df282bd1010
Eric Kim <kim2000@uw.edu>
2020-04-22 16:21:50 +0900

    Fix errors

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
  * Section: CSE 154 AK
  *
  * This is the JS to manipulate the UI for of the homepage with user's interactions.
- * This JS also allows the user to change the brush size and draw pixels 
+ * This JS also allows the user to change the brush size and draw pixels
  * on the square 'sketchpad' in the main homepage.
  */
 
 'use strict';
 (function() {
+  const CANVAS_SIZE = 400;
   window.addEventListener('load', init);
-  
   /**
    * Sets up the sketchpad/canvas with the default 'medium' brush size.
    */
@@ -20,17 +20,22 @@
     let context = canvas.getContext('2d');
     let toolButton = document.getElementById('tool-button');
     let brushButton = document.getElementById('brush-button');
-    let brushSize = 10;
-    let pixelSize = canvas.width/brushSize;
+    let selectedBrushAtStart = false;
+    let pixelSize;
     canvas.context = context;
+    canvas.width = canvas.height = CANVAS_SIZE;
 
     brushButton.addEventListener('click', function() {
-      pixelSize = canvas.width/setBrush();
+      let brushSize = setBrush();
+      selectedBrushAtStart = true;
+      pixelSize = CANVAS_SIZE / brushSize;
     });
 
-    canvas.addEventListener('click', function (e) {
-      let isEraser = checkEraser(toolButton);
-      drawPixel(e, this, isEraser, pixelSize);
+    canvas.addEventListener('click', function (evt) {
+      if (selectedBrushAtStart) {
+        let isEraser = checkEraser(toolButton);
+        drawPixel(evt, this, isEraser, pixelSize);
+      }
     });
 
     toolButton.addEventListener('click', changeTool);
@@ -38,47 +43,50 @@
 
   /**
    * Draws a square box of specificed size on the canvas based on the user's mouse location.
-   * @param {Object} e - A mouse event after the user's click interaction.
+   * @param {Object} evt - A mouse event after the user's click interaction.
    * @param {Object} canvas - The canvas to draw the pixels in.
    * @param {boolean} isEraser - Rather or not the eraser tool is being used.
    * @param {int} pixelSize - The size of the individual pixels.
    */
-  function drawPixel(e, canvas, isEraser, pixelSize) {
-    let pos = getMouseLocation(e, canvas);
-    let nthCol = Math.floor(pos.x/pixelSize);
-    let nthRow = Math.floor(pos.y/pixelSize);
-    isEraser ? canvas.context.clearRect(nthCol*pixelSize, nthRow*pixelSize, pixelSize, pixelSize)
-             : canvas.context.fillRect(nthCol*pixelSize, nthRow*pixelSize, pixelSize, pixelSize);
+  function drawPixel(evt, canvas, isEraser, pixelSize) {
+    let pos = getMouseLocation(evt, canvas);
+    let nthCol = Math.floor(pos.xCord / pixelSize);
+    let nthRow = Math.floor(pos.yCord/ pixelSize);
+    if (isEraser) {
+      canvas.context.clearRect(nthCol * pixelSize, nthRow * pixelSize, pixelSize, pixelSize);
+    } else {
+      canvas.context.fillRect(nthCol * pixelSize, nthRow * pixelSize, pixelSize, pixelSize);
+    }
   }
 
   /**
    * Calculates and returns the x and y coordinates of the mouse relative to the canvas.
-   * @param {Object} e - A mouse event after the user's click interaction.
+   * @param {Object} evt - A mouse event after the user's click interaction.
    * @param {Object} canvas - The canvas to draw the pixels in.
    * @return {Object} The coordinates of the mouse relative to the canvas.
    */
-  function getMouseLocation(e, canvas) {
+  function getMouseLocation(evt, canvas) {
     let canvasRect = canvas.getBoundingClientRect();
     return {
-      x: e.clientX - canvasRect.left,
-      y: e.clientY - canvasRect.top
+      xCord: evt.clientX - canvasRect.left,
+      yCord: evt.clientY - canvasRect.top
 
     }
   }
 
   /**
-   * Changes the tool that the user will use on canvas. 
-   * @param {Object} e - A mouse event after the user's click interaction.
+   * Changes the tool that the user will use on canvas.
+   * @param {Object} evt - A mouse event after the user's click interaction.
    */
-  function changeTool(e) {
+  function changeTool(evt) {
     let userAction = document.querySelector('h2');
-    let isEraser = checkEraser(e.target);
+    let isEraser = checkEraser(evt.target);
     if (isEraser) {
       userAction.innerText = 'Painting';
-      e.target.innerText = 'Eraser';
+      evt.target.innerText = 'Eraser';
     } else {
       userAction.innerText = 'Erasing';
-      e.target.innerText = 'Paint';
+      evt.target.innerText = 'Paint';
     }
   }
 
@@ -93,20 +101,21 @@
 
   /**
    * Returns the selected brush size from radio buttons and adds a new button
-   * in the document to change the brush size. 
+   * in the document to change the brush size.
    * @return {int} The size of the selected brush.
    */
   function setBrush() {
     let radios = document.getElementsByName('brush-size');
     let sketchpad = document.querySelector('#sketchpad > div');
+    let action = document.querySelector('#sketchpad > h2');
     let backButton = document.createElement('button');
     let brushSelections = document.getElementById('brush-section');
+    let toolButton = document.getElementById('tool-button');
     let size;
 
-    for (var i = 0, length = radios.length; i < length; i++) {
+    for (let i = 0; i < radios.length; i++) {
       if (radios[i].checked) {
         size = radios[i].value;
-        break;
       }
     }
 
@@ -115,11 +124,13 @@
     backButton.addEventListener('click', function() {
       brushSelections.classList.remove('hidden');
       this.parentNode.removeChild(this);
-    })
-    
+      action.classList.add('hidden');
+      toolButton.classList.add('hidden');
+    });
     sketchpad.appendChild(backButton);
     brushSelections.classList.add('hidden');
-
+    action.classList.remove('hidden');
+    toolButton.classList.remove('hidden');
     return size; 
   }
 })();
