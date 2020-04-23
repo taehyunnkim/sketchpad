9a9ef5b131ec38c4a7dcde3b513720794b1271f6
Eric Kim <kim2000@uw.edu>
2020-04-23 14:15:05 +0900

    Fix commenting

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * Date: April 19, 2020
  * Section: CSE 154 AK
  *
- * This is the JS to manipulate the UI for of the homepage with user's interactions.
+ * This is the JS to manipulate the UI of the homepage with user's interactions.
  * This JS also allows the user to change the brush size and draw pixels
- * on the square 'sketchpad' in the main homepage.
+ * on the square 'sketchpad' that is presented in the main homepage.
  */
 
 'use strict';
@@ -28,7 +28,7 @@
     canvas.height = CANVAS_SIZE;
 
     brushButton.addEventListener('click', function() {
-      let brushSize = setBrush();
+      let brushSize = getBrushSize();
       selectedBrushAtStart = true;
       pixelSize = CANVAS_SIZE / brushSize;
       moveToSketchMode();
@@ -37,7 +37,7 @@
     canvas.addEventListener('click', function(evt) {
       if (selectedBrushAtStart) {
         let isEraser = checkEraser(toolButton);
-        drawPixel(evt, this, isEraser, pixelSize);
+        updateCanvas(evt, this, isEraser, pixelSize);
       }
     });
 
@@ -45,13 +45,14 @@
   }
 
   /**
-   * Draws a square box of specificed size on the canvas based on the user's mouse location.
-   * @param {Object} evt - A mouse event after the user's click interaction.
+   * Draws or erases a square box/pixel of specified size on
+   * the canvas based on the user's mouse click location.
+   * @param {Object} evt - The mouse event after the user's click interaction.
    * @param {Object} canvas - The canvas to draw the pixels in.
    * @param {boolean} isEraser - Rather or not the eraser tool is being used.
    * @param {int} pixelSize - The size of the individual pixels.
    */
-  function drawPixel(evt, canvas, isEraser, pixelSize) {
+  function updateCanvas(evt, canvas, isEraser, pixelSize) {
     let pos = getMouseLocation(evt, canvas);
     let nthCol = Math.floor(pos.xCord / pixelSize);
     let nthRow = Math.floor(pos.yCord / pixelSize);
@@ -64,7 +65,7 @@
 
   /**
    * Calculates and returns the x and y coordinates of the mouse relative to the canvas.
-   * @param {Object} evt - A mouse event after the user's click interaction.
+   * @param {Object} evt - The mouse event after the user's click interaction.
    * @param {Object} canvas - The canvas to draw the pixels in.
    * @return {Object} The coordinates of the mouse relative to the canvas.
    */
@@ -78,7 +79,7 @@
 
   /**
    * Changes the tool that the user will use on canvas.
-   * @param {Object} evt - A mouse event after the user's click interaction.
+   * @param {Object} evt - The mouse event after the user's click interaction.
    */
   function changeTool(evt) {
     let userAction = document.querySelector('h2');
@@ -94,7 +95,7 @@
 
   /**
    * Checks if the user is using the eraser tool or the paint tool.
-   * @param {Object} toolButton - A button for the user to switch their tool.
+   * @param {Object} toolButton - The button for the user to switch their tool.
    * @return {boolean} True if the user is erasing; False if the user is painting.
    */
   function checkEraser(toolButton) {
@@ -103,9 +104,9 @@
 
   /**
    * Returns the selected brush size from radio buttons.
-   * @return {int} The size of the selected brush.
+   * @return {int} The size of the selected brush from radio buttons.
    */
-  function setBrush() {
+  function getBrushSize() {
     let radios = document.getElementsByName('brush-size');
     let size;
 
@@ -121,8 +122,9 @@
   /**
    * Hides the brush selections section from the document.
    * Reveals user action and tools in the document.
-   * Adds a button that, when clicked, hides user action and tools,
-   * and reveals the brush selections again.
+   * Adds a button to the document that, when clicked,
+   * hides user action and tools, and reveals
+   * the brush selections section again.
    */
   function moveToSketchMode() {
     let sketchpad = document.querySelector('#sketchpad > div');
